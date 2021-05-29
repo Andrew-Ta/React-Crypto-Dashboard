01b89edd18efec7ac52ae412621ea4c55367ec9c
Andrew Ta <65432747+Andrew-Ta@users.noreply.github.com>
2021-05-28 20:54:21 -0600

    update

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components'
+import {Link} from 'react-router-dom'
+
+// import Logo from 'components/icons/Logo'
+
+const HomePageStyles = styled.header `
+    text-align:center;
+    margin: 6rem auto 0;
+
+    svg{
+        width: 80px;
+    }
+    h1{
+        font-size: 2.25rem;
+    }
+    p{
+        color:#76748a;
+    }
+`
+
+const HomePage = (props) => {
+    return ( 
+
+        <HomePageStyles>
+            {/* <Logo/> */}
+            <h1>Home Page</h1>
+            <p>Focus on people and we'll focus on the paperwork</p>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/register">Register</Link></li>
+        </HomePageStyles>
+
+     );
+}
+ 
+export default HomePage;
